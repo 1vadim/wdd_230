99f2eb043e730f21d5abf90fac674e37791f2dbb
1Vadim <2332399@gmail.com>
2022-01-25 21:10:10 +0200

    Changed *.js name and <width> of <body>

--- lesson3/js/current_time.js
+++ lesson3/js/current_time.js
@@ -2,4 +2,4 @@ let d = new Date();
 document.querySelector('#LastMod').innerHTML = `Last updated: ${document.lastModified}`;
 
 
-document.querySelector('#year').textContent = d.getFullYear();;
+document.querySelector('#year').textContent = d.getFullYear();
